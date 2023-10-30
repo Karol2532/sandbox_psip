3d28e4e64e2fac6744381fe116f6a310377093e2
Karol2532 <149382213+Karol2532@users.noreply.github.com>
2023-10-30 14:45:48 +0100

    add new file with list

---
+++ dane.py
@@ -0,0 +1,7 @@
+users_list: list = [
+{"name":"Karol","nick":"plutas","posts":731},
+{"name": "Kacper", "nick": "szysza","posts":6731},
+{"name":"Mateusz","nick":"świetlik","posts":69},
+{"name":"Bartosz","nick":"baran","posts":357325},
+{"name":"Jakub","nick":"kremerciok","posts":2526}
+]
---
+++ main.py
@@ -0,0 +1,7 @@
+from dane import users_list
+
+for user in users_list:
+    print(f'Twój znajomy {user["nick"]} dodał {user["posts"]} postów')
+
+
+
