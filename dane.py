e1a17fb1eb849777ac2ed89e64b0384bb9d2d521
Karol2532 <149382213+Karol2532@users.noreply.github.com>
2023-11-06 09:23:39 +0100

    add data
    dodałem nowych użytkowników do testów

--- dane.py
+++ dane.py
@@ -3,5 +3,8 @@ users_list: list = [
 {"name": "Kacper", "nick": "szysza","posts":6731},
 {"name":"Mateusz","nick":"świetlik","posts":69},
 {"name":"Bartosz","nick":"baran","posts":357325},
-{"name":"Jakub","nick":"kremerciok","posts":2526}
+{"name":"Jakub","nick":"kremerciok","posts":2526},
+{"name":"Agata","nick":"rzygalo","posts":232146},
+{"name":"Ola","nick":"jaciub","posts":982146},
+{"name":"Kinga","nick":"kowal","posts":46}
 ]
