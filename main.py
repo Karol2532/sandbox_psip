44eedb950cdd3fb1ae70380cee8e892df1d49d78
Karol2532 <149382213+Karol2532@users.noreply.github.com>
2023-11-06 12:56:38 +0100

    add new function

--- main.py
+++ main.py
@@ -1,7 +1,18 @@
 from dane import users_list
 
+def add_user_to(users_list: list) -> None:
+    """
+    add object to list
+    :param users_list: list - user list
+    :return: None
+    """
+    name = input('podaj imię')
+    nick = input('podaj nick')
+    posts = input('podaj liczbę postów')
+    users_list.append({"name": name, "nick": nick, "posts": posts})
+
+add_user_to(users_list)
+add_user_to(users_list)
+
 for user in users_list:
-    print(f'Twój znajomy {user["nick"]} dodał {user["posts"]} postów')
-
-
-
+    print(f'Twój znajomy {user["name"]} dodał {user["posts"]} postów')
