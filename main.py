b42ac6d82a02879350b4aeb888bc5e0a660ad4b0
Karol2532 <149382213+Karol2532@users.noreply.github.com>
2023-12-11 23:02:49 +0100

    wip:crud_dla_bazy_danych

--- main.py
+++ main.py
@@ -3,7 +3,15 @@ from untils.my_functions import gui, add_user_to, remove_user_from,show_users_fr
 
 gui(users_list)
 
-
+# TODO dodac tabele do bazy danych reprezentujaca uzytkownika
+# TODO wlaczyc do kodu obsluge bazy danych
+# TODO napisac klase uzytkownika o strukturze zgodnej z user_list (sama definicja)
+# TODO sprawko z ww do 20.12 (sroda)
+# ma zawierac: tytulowa
+# spis
+# opis kodu realiujacego zadanie rysowanie mapy uwzgledniajacy poszczegolne fiunkcje oraz ich skladnie
+# zredaliwowany kod na Git
+# podsumowanie i wsnioski koncowe, ss
 
 
 
--- notateczki.py
+++ notateczki.py
@@ -6,7 +6,47 @@ db_params = sqlalchemy.URL.create(
     password='123',
     host='localhost',
     database='postgres',
-    port=5432
+    port=5433
 )
+
+
+
 engine = sqlalchemy.create_engine(db_params)
 connection = engine.connect()
+
+#sql_query_1 = sqlalchemy.text("INSERT INTO public.my_table(name) VALUES ('koc');")
+#sql_query_1 = sqlalchemy.text("select * from public.my_table;")
+
+#user = input('Podaj nazwę użytkownika do usunięcia')
+#sql_query_1 = sqlalchemy.text(f"DELETE FROM public.my_table WHERE name ='{user}';")
+
+#kogo_zamienic = input('Podaj nazwę użytkownika do zamiany')
+#na_kogo = input('Podaj na kogo zamienić')
+#sql_query_1 = sqlalchemy.text(f"UPDATE public.my_table SET name='{na_kogo}' WHERE name='{kogo_zamienic}';")
+user = 'laska'
+def dodaj_użytkownika(user:str):
+    sql_query_1 = sqlalchemy.text("INSERT INTO public.my_table(name) VALUES ('{user}');")
+    connection.execute(sql_query_1)
+    connection.commit()
+
+
+def usuń_użytkownika(user:str):
+    sql_query_1 = sqlalchemy.text(f"DELETE FROM public.my_table WHERE name ='{user}';")
+    connection.execute(sql_query_1)
+    connection.commit()
+
+
+def aktualizuj_użytkownika(user_1:str, user_2:str):
+    sql_query_1 = sqlalchemy.text(f"UPDATE public.my_table SET name='{user_2}' WHERE name='{user_1}';")
+    connection.execute(sql_query_1)
+    connection.commit()
+
+aktualizuj_użytkownika(
+    user_1=input('Podaj nazwę użytkownika do zamiany'),
+    user_2=input('Podaj na kogo zamienić')
+)
+
+
+
+#connection.execute(sql_query_1)
+#connection.commit()
