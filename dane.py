fe0213df54539fde74df729f6408986e73cc23af
Karol2532 <149382213+Karol2532@users.noreply.github.com>
2023-12-19 15:16:59 +0100

    progress

--- dane.py
+++ dane.py
@@ -1,11 +1,11 @@
 users_list: list = [
-{"name":"Karol","nick":"plutas","posts":731},
-{"name": "Kacper", "nick": "szysza","posts":6731},
-{"name":"Mateusz","nick":"świetlik","posts":69},
-{"name":"Bartosz","nick":"baran","posts":357325},
-{"name":"Jakub","nick":"kremerciok","posts":2526},
-{"name":"Agata","nick":"rzygalo","posts":232146},
-{"name":"Ola","nick":"jaciub","posts":982146},
-{"name":"Ola","nick":"kierownik","posts":986},
-{"name":"Kinga","nick":"kowal","posts":46}
+{"name":"Karol","nick":"plutas","city":"Warszawa","posts":731},
+{"name": "Kacper", "nick": "szysza","city":"Warszawa","posts":6731},
+{"name":"Mateusz","nick":"świetlik","city":"Warszawa","posts":69},
+{"name":"Bartosz","nick":"baran","city":"Warszawa","posts":357325},
+{"name":"Jakub","nick":"kremerciok","city":"Warszawa","posts":2526},
+{"name":"Agata","nick":"rzygalo","city":"Warszawa","posts":232146},
+{"name":"Ola","nick":"jaciub","city":"Warszawa","posts":982146},
+{"name":"Ola","nick":"kierownik","city":"Warszawa","posts":986},
+{"name":"Kinga","nick":"kowal","city":"Warszawa","posts":46}
 ]
---
+++ dml.py
@@ -0,0 +1,38 @@
+import sqlalchemy
+import os
+import sqlalchemy.orm
+import random
+from faker import Faker
+from geoalchemy2 import Geometry
+from dotenv import load_dotenv
+
+load_dotenv()
+
+db_params = sqlalchemy.URL.create(
+    drivername='postgresql+psycopg2',
+    username=os.getenv('POSTGRES_USER'),
+    password=os.getenv('POSTGRES_PASSWORD'),
+    host=os.getenv('POSTGRES_HOST'),
+    database=os.getenv('POSTGRES_DB'),
+    port=os.getenv('POSTGRES_PORT')
+)
+
+engine = sqlalchemy.create_engine(db_params)
+connection = engine.connect()
+
+Base = sqlalchemy.orm.declarative_base()
+
+#trzeba pamietac o podaniu extension
+class User(Base):
+    __tablename__ = 'mytable'
+
+    id = sqlalchemy.Column(sqlalchemy.Integer(), primary_key=True)
+    name = sqlalchemy.Column(sqlalchemy.String(100), nullable=True)
+    location = sqlalchemy.Column('geom', Geometry(geometry_type='POINT', srid=4326), nullable=True)
+
+
+Base.metadata.create_all(engine)
+
+session.flush()
+connection.close()
+engine.dispose()
